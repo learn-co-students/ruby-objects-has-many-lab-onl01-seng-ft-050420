13204b92ea2f25cf4224b497898497f3558184b3
michael thomas <michael.thomas2014@gmail.com>
2020-05-19 18:07:06 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs
+    Song.all.select{|song| song.artist == self} 
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song 
+  end
+  
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    add_song(song_name)
+  end
+  
+end
